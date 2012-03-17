71a4e11a99dac33ff4aceb5a3f520253403c637f
agustin.gabiola <agustingabiola@hotmail.com>
2012-03-17 08:35:13 -0300

    commit day 2

--- HTML5/day2/index.php
+++ HTML5/day2/index.php
@@ -2,7 +2,7 @@
 <html> 
 	<head> 
 	<title>My Page</title> 
-	<meta name="viewport" content="width=device-width, initial-scale=0.3, maximum-scale=1.0, user-scalable=yes"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=yes"> 
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
