05d0bbf3b8184106698b8e42fa060544f6a78efc
agustin.gabiola <agustingabiola@hotmail.com>
2012-04-14 18:03:54 -0300

    menos <div>`s

--- HTML5/day91011/Scripts/templateMovie.js
+++ HTML5/day91011/Scripts/templateMovie.js
@@ -2,19 +2,16 @@
  * Movie template for backbone
  */
 var movieTemplateAux =  '<div id="peli">\
-                        <div><%= title %> <label>(<%= year %>)</label> </div>\
-                        <div><%= details %></div>\
-                    <div></div>\
+                        <%= title %> <label>(<%= year %>)</label>\
+                        <%= details %>\
                     </div>\
                     <div id="peli-editar">\
-                    <div>\
                         <input id="titulo" type="text" value="<%= title %>" />\
                         <label>\
                         (<input id="year" type="text" value="<%= year %>" />)\
                         </label>\
                     </div>\
-                    <div><input id="details" type="text" value="<%= details %>" /></div>\
-                    <div></div>\
+                        <input id="details" type="text" value="<%= details %>" />\
                     </div>';
 //creo que esto hace que no ande..
 
